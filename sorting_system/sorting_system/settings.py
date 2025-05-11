648dc03bee571f41ad344eea901727ef9f5c71e8
Alexa6748 <sandjieva.alex@gmail.com>
2025-05-12 01:30:20 +0300

    core app

--- sorting_system/sorting_system/settings.py
+++ sorting_system/sorting_system/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'classification',
+    'detection',
+    'monitoring',
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
